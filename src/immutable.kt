ddaa10237249e6a829205e4f4d2009f71d46628f
gwanhyeon <kgh940525@gmail.com>
2020-09-17 00:45:35 +0900

    Add immutable and mutable

---
+++ src/immutable.kt
@@ -0,0 +1,7 @@
+fun main ( args : Array < String >) {
+
+    val myName = "KGH"
+    //myName = "gwanhyeon kim"
+    // 변경시 컴파일 에러 발생
+    println ( "내 이름 :" + myName )
+}
---
+++ src/mutable.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    var myName = "KGH"
+    myName = "gwanhyeon Kim"
+    println("My name " + myName);
+    
+}
