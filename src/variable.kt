dff6376a1186adffc9871b5a8530a1821c7211d1
gwanhyeon <kgh940525@gmail.com>
2020-09-17 00:56:06 +0900

    Add Kotlin dataType

---
+++ src/variable.kt
@@ -0,0 +1,52 @@
+fun main(args: Array<String>) {
+    //var book = "gwanhyeon kim"
+    var book : String = "gwanhyeon kim"
+
+    var book1 : String
+    book1 = "gwanhyeon Kim"
+
+
+    // 1. byte
+
+    val num : Byte = 99;
+    println("$num")
+
+    // 2. byte max , min
+    var bigByte : Byte = Byte . MAX_VALUE
+    var smallByte : Byte = Byte . MIN_VALUE
+    println ( "가장 큰 바이트 값 : + $bigByte" )
+    println ( "가장 작은 바이트 값 : $smallByte" )
+
+    // 3. short
+
+    val num1 : Short = 29999
+    println("$num1")
+
+    // 4. int
+    var num2 : Int = 123456
+    println("$num2")
+
+    // 5. Long
+    var num3 : Long = 12345678
+    println("$num")
+
+    // 6.Double
+    var num4 : Double = 101.99
+    println("$num")
+
+    // 7. Float
+    var num5 = 101.99F;
+    println("$num")
+
+    // 8. Boolean
+    var boolValue = false
+    println("$boolValue")
+
+    // 9. Char
+    var ch = 'A'
+    println("$ch")
+    var ch2 : Char
+    ch2 = 'Z'
+    println("$ch2")
+
+}
